47d335fb85e0ed76e7f515f37c781361261533f6
Reese Kneeland <77468346+reesekneeland@users.noreply.github.com>
2022-06-20 19:17:03 -0700

    Delete labs directory

--- labs/lab04_polymorphism/entity.cc
+++
@@ -1,9 +0,0 @@
-#include "entity.h"
-
-const std::string& Entity::GetName() {
-    // static allows us to keep the name entity for the duration
-    // of the entire program.  This allows us to pass a reference back.
-    // When a variable is declared static, there is only one variable per class.
-    static std::string name("Entity");
-    return name;
-}
